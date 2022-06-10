d23b7b11c4433c35bd6d8662f23e6576c35a6338
Marcelo Luna <luna.marceloandres@gmail.com>
2022-06-10 18:00:30 -0300

    MainMenu WIP

---
+++ TesisDV/Assets/Scripts/UI/MainMenu/MainScreen.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainScreen : MonoBehaviour
+{
+    public GameObject credits;
+    public GameObject houseStructure;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BtnPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void BtnCredits()
+    {
+        houseStructure.SetActive(false);
+        credits.SetActive(!credits.activeSelf);
+    }
+    public void BtnBack()
+    {
+        houseStructure.SetActive(true);
+        credits.SetActive(!credits.activeSelf);
+    }
+    public void BtnQuit()
+    {
+        Application.Quit();
+    }
+
+}
