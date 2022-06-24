4afec4de20295e5291c176912c4c9366a23c0ab1
GustavoHerreraDa <gustavo.herrera@davinci.edu.ar>
2022-06-24 16:24:26 -0300

    Mini Map another part

---
+++ TesisDV/Assets/Scripts/MiniMap/MiniMapCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Player _player;
+    private float smoothing = 20f;
+    private Vector3 offset = new Vector3(0f, 10, 0f);
+
+    void Awake()
+    {
+        _player = GameObject.Find("Player").GetComponent<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _player.transform.position + offset, smoothing * Time.deltaTime);
+    }
+}
