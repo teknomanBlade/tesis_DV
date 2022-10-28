cba7375e1c42bf2b564c9b6db9201f2c6de66ccf
Marcelo Luna <luna.marceloandres@gmail.com>
2022-10-27 23:18:11 -0300

    Improvements and addons
    - New BasementWalls and Floor
    - New ItemSpawner Waves

--- TesisDV/Assets/Scripts/ItemSpawner.cs
+++ TesisDV/Assets/Scripts/ItemSpawner.cs
@@ -7,6 +7,9 @@ public class ItemSpawner : MonoBehaviour
     [SerializeField] private GameObject _firstRoundItems;
     [SerializeField] private GameObject _secondRoundItems;
     [SerializeField] private GameObject _thirdRoundItems;
+    [SerializeField] private GameObject _fourthRoundItems;
+    [SerializeField] private GameObject _fifthRoundItems;
+    [SerializeField] private GameObject _sixthRoundItems;
 
     void Start()
     {
@@ -14,6 +17,9 @@ public class ItemSpawner : MonoBehaviour
         _firstRoundItems =  transform.GetChild(0).gameObject;
         _secondRoundItems = transform.GetChild(1).gameObject;
         _thirdRoundItems =  transform.GetChild(2).gameObject;
+        _fourthRoundItems = transform.GetChild(3).gameObject;
+        _fifthRoundItems = transform.GetChild(4).gameObject;
+        _sixthRoundItems = transform.GetChild(5).gameObject;
     }
 
     private void SpawnItems(int currentRound)
@@ -21,16 +27,27 @@ public class ItemSpawner : MonoBehaviour
         switch(currentRound)
         {
             case 0:
-                    _firstRoundItems.SetActive(true);
+                _firstRoundItems.SetActive(true);
             break;
             case 1:
                     //_firstRoundItems.SetActive(false);
-                    _secondRoundItems.SetActive(true);
+                _secondRoundItems.SetActive(true);
             break;
             case 2:
                     //_secondRoundItems.SetActive(false);
-                    _thirdRoundItems.SetActive(true);
+                _thirdRoundItems.SetActive(true);
             break;
+            case 3:
+                _fourthRoundItems.SetActive(true);
+                break;
+            case 4:
+                //_firstRoundItems.SetActive(false);
+                _fifthRoundItems.SetActive(true);
+                break;
+            case 5:
+                //_secondRoundItems.SetActive(false);
+                _sixthRoundItems.SetActive(true);
+                break;
         }
     }
 }
